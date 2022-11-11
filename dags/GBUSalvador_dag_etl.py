83db082575342377b1386e29bcd9c09c3a06f08f
Aldo Agunin <aldoagunin@gmail.com>
2022-11-11 12:34:13 -0300

    Story 4 (extraccion) version final van los 2 DAGs (en dags) y 2 csv (en files)

--- dags/GBUSalvador_dag_etl.py
+++ dags/GBUSalvador_dag_etl.py
@@ -3,55 +3,63 @@
 ## USalvador
 
 COMO: Analista de datos
-QUIERO: Configurar los log
-PARA: Mostrarlos en consola
+QUIERO: Implementar Python Operator para Extracción
+PARA: tomar los datos de las bases de datos en el DAG
 
 Criterios de aceptacion: 
-- Configurar logs para Univ. Nacional Del Comahue
-- Configurar logs para Universidad del Salvador
-- Utilizar la librería de Loggin de python: https://docs.python.org/3/howto/logging.html
-- Realizar un log al empezar cada DAG con el nombre del logger
-- Formato del log: %Y-%m-%d - nombre_logger - mensaje
-Aclaracion:
-Deben dejar la configuracion lista para que se pueda incluir dentro de las funciones futuras. 
-No es necesario empezar a escribir logs.
+Configurar un Python Operator, para que extraiga informacion de la base de datos 
+utilizando el .sql disponible en el repositorio base de las siguientes universidades: 
+- Univ. Nacional Del Comahue
+- Universidad Del Salvador
+Dejar la información en un archivo .csv dentro de la carpeta files.
+Documentacion
+https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/operators/python/index.html#module-airflow.operators.python
+Utilizar el provider https://airflow.apache.org/docs/apache-airflow-providers-postgres/stable/_api/airflow/providers/postgres/hooks/postgres/index.html 
+Analizar implementacion sugerida en este post:
+https://stackoverflow.com/questions/72165393/use-result-from-one-operator-inside-another
+
 
 # Dev: Aldo Agunin
-# Fecha: 05/11/2022
+# Fecha: 07/11/2022
 """
 
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from pathlib import Path
 import logging
 import logging.config
+import pandas as pd
 
-# ------- DECLARACIONES ----------------------
-universidad_corto = "USalvador"
-universidad_largo = "Universidad del Salvador"
+# ------- DECLARACIONES -----------
+universidad_corto = 'USalvador'
+universidad_largo = 'Universidad del Salvador'
 
-# ------- LOGGER -----------------------------
+# ------- LOGGER ------------------
 log_cfg = ('GB' + universidad_corto + '_log.cfg')
 configfile = Path(__file__).parent.parent / 'plugins' / log_cfg
 logging.config.fileConfig(configfile, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-#---------------------------------------------
-def prueba_log():
-    logger.info('*** Comienzo de la Prueba de Logger ***')
-    logger.debug("debug message")
-    logger.info("info message")
-    logger.warning("warn message")
-    logger.error("error message")
-    logger.critical("critical message")
-    logger.info('*** Fin de la Prueba de Logger ***')
-#---------------------------------------------
+#---------------  extraccion  --------------
+logger.info('*** Comenzando Extracción ***')
+def datos_a_csv():
+    ## Ubicacion del .sql
+    sql_path = Path(__file__).parent.parent / 'include' / ('GB' + universidad_corto + '.sql')
+    ## Leo el .sql
+    sql_consulta = open(sql_path, 'r').read()
+    ## Conexion a la base
+    hook = PostgresHook(postgres_conn_id='alkemy_db')
+    conexion = hook.get_conn()
+    df = pd.read_sql(sql_consulta, conexion)
+    ## Guardo .csv
+    csv_path = Path.cwd() / 'files' / ('GB' + universidad_corto + '_select.csv')
+    df.to_csv(csv_path, index=False)
+    return
+#------------------------------------------
 
-# forma alternativa al WITH DAG
-# PENDIENTE Hacerlo con Taskflow (decoradores)
-# https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html
 dag = DAG(
     dag_id=f'GB{universidad_corto}_dag_etl',   # dag_id='GBUSalvador_dag_etl',
     description=f'DAG para hacer ETL de la {universidad_largo}',
@@ -61,33 +69,20 @@ dag = DAG(
     catchup=False,
     default_args={
         'retries': 5, # If a task fails, it will retry 5 times.
-        'retry_delay': timedelta(minutes=1),
+        'retry_delay': timedelta(minutes=5),
         },
-    )
-
-# task1 = EmptyOperator(
-#     task_id="task_1", # debe ser único dentro del dag
-#     dag=dag # dag al que pertenece esta tarea
-#     )
-
-# task1 = PythonOperator(
-#     task_id="task_1", # debe ser único dentro del dag
-#     python_callable=function_task_1, # función que se ejecutará
-#     dag=dag # dag al que pertenece esta tarea
-#     )
-
-# Provisoriamente agrego la tarea "begin" para probar el logger
-begin = PythonOperator(
-        task_id='begin',
-        python_callable=prueba_log,
-        dag=dag,
     )
 
 # primera tarea: correr script .SQL y la salida a .CSV
 # se usara un PythonOperator que ejecute la consulta .SQL de la Story1 y genere salida a .CSV
-extract = EmptyOperator(
-    task_id="extraction_task",
-    dag=dag
+# extract = EmptyOperator(
+#     task_id="extraction_task",
+#     dag=dag
+#     )
+extract = PythonOperator(
+        task_id='extraction_task',
+        python_callable=datos_a_csv,
+        dag=dag,
     )
 
 # segunda tarea: procesar datos en pandas
@@ -104,4 +99,4 @@ load = EmptyOperator(
     dag=dag
     )
 
-begin >> extract >> transform >> load
+extract >> transform >> load
